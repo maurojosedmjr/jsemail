6bfd5b60c377635acc052e9830232f771b494b8d
maurojosedmjr <maurojosedmjr@gmail.com>
2017-11-14 17:16:17 -0200

    post form

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var url = require('url');
 var fs = require('fs');
-var restify = require('restify');
+var qs = require('querystring');
 
 function renderHtml(path, res){
   fs.readFile(path, null, function(error, data) {
@@ -14,6 +14,24 @@ function renderHtml(path, res){
   });
 }
 
+function getForm(req, res){
+  if (req.method === 'POST'){
+    var body = '';
+    req.on('data', function(chunk){
+      body += chunk;
+    });
+    req.on('end', function() {
+      var data = qs.parse(body);
+      res.writeHead(200);
+      console.log(JSON.stringify(data));
+      res.end(JSON.stringify(data));
+    })
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+}
+
 module.exports = {
   handleRequest: function (req, res) {
     res.writeHead(200, {'Content-type': 'text/html'});
@@ -22,6 +40,9 @@ module.exports = {
     switch (path) {
       case '/':
         renderHtml('./index.html', res);
+        break;
+      case '/post':
+        getForm(req, res);
         break;
       default:
         res.writeHead(404);
