121698b2b7080f5ae2274cc3691dd8c2af4930fe
maurojosedmjr <maurojosedmjr@gmail.com>
2017-11-14 16:24:09 -0200

    implementando handleRequests

---
+++ app.js
@@ -0,0 +1,32 @@
+var url = require('url');
+var fs = require('fs');
+var restify = require('restify');
+
+function renderHtml(path, res){
+  fs.readFile(path, null, function(error, data) {
+    if (error){
+      res.writeHead(404);
+      res.write('Arquivo nao encontrado!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+}
+
+module.exports = {
+  handleRequest: function (req, res) {
+    res.writeHead(200, {'Content-type': 'text/html'});
+
+    var path = url.parse(req.url).pathname;
+    switch (path) {
+      case '/':
+        renderHtml('./index.html', res);
+        break;
+      default:
+        res.writeHead(404);
+        res.write('Caminho nao encontrado');
+        red.end();
+    }
+  }
+}
